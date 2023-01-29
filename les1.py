83397388e3478c41f85796d24baedc8cabd1953e
Anton Kolodin <anton.kolodin2012@yandex.ru>
2023-01-29 15:07:05 +0300

    task 1 and 3 done

--- les1.py
+++
@@ -1,6 +0,0 @@
-# lesson 1
-
-print('Введите желаемое трехзначное число: ', end='')
-num = input()
-result = int(num[0]) + int(num[1]) + int(num[2])
-print('Сумма цифр введенного числа составляет: ', result)
