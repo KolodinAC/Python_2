a8f7c9b121a4ab934c3d11b028aaa1d4365bf3e4
Anton Kolodin <anton.kolodin2012@yandex.ru>
2023-01-29 14:31:23 +0300

    hw1 done

---
+++ hw1.py
@@ -0,0 +1,6 @@
+# lesson 1
+
+print('Введите желаемое трехзначное число: ', end='')
+num = input()
+result = int(num[0]) + int(num[1]) + int(num[2])
+print('Сумма цифр введенного числа составляет: ', result)
